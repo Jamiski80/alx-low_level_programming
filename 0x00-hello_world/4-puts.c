e62d09537fc7138a381f1a4a4c97020f78a474b3
Nweke James <jnweke37@gmail.com>
2022-10-17 09:01:48 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Print a line of code using puts.
+ * main - Entry point
  *
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
